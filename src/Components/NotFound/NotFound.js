cf4917b23f0f865b2740fb0580b09c1a6b888a96
khanmuhammadhridoy <khanmuhammadhridoy@gmail.com>
2021-03-09 23:09:30 +0600

    first commit

---
+++ src/Components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div style={{textAlign:"center"}}>
+            <h1>404 Error</h1>
+            <h3>Not Found</h3>
+        </div>
+    );
+};
+
+export default NotFound;
